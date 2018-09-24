1dab414b2385ec0ce2196d9cbd47eaaf267c206d
Vinod Jembu <vinod.jembu@datastax.com>
2018-09-24 15:55:15 -0400

    Updated with comments

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,21 @@
-# Uncomment resources below and add required arguments.
 
-resource "aws_security_group" "dse_sec_grp" {
-  name = "dse_sec_grp"
+#AWS Provider access deatils (AWS)
+
+provider "aws" {
+    access_key = "Enter AWS ACCESS KEY"
+    secret_key = "Enter AWS SECRET KEY"
+    region = "us-east-1"
+}
+
+
+#Un-Comment if you want to launch instance with new security group or applying new rules
+
+/*
+resource "aws_security_group" "dse" {
+  name = "dse"
   description = "dse security group- allow all"
+
+  #your virtual private cloud
   vpc_id = "vpc-001394d0ecbcdfae1"
 }
 
@@ -12,7 +25,7 @@ resource "aws_security_group_rule" "ssh_ingress_access" {
   to_port = 22
   protocol = "tcp"
   cidr_blocks = [ "0.0.0.0/0" ] 
-  security_group_id = "${aws_security_group.dse_sec_grp.id}"
+  security_group_id = "${aws_security_group.dse.id}"
 }
 
 resource "aws_security_group_rule" "egress_access" {
@@ -21,19 +34,38 @@ resource "aws_security_group_rule" "egress_access" {
   to_port = 65535
   protocol = "tcp"
   cidr_blocks = [ "0.0.0.0/0" ]
-  security_group_id = "${aws_security_group.dse_sec_grp.id}"
+  security_group_id = "${aws_security_group.dse.id}"
 }
+*/
 
-resource "aws_instance" "jembu-node" {
-  instance_type = "t2.xlarge"
-  vpc_security_group_ids = [ "${aws_security_group.dse_sec_grp.id}" ]
-  associate_public_ip_address = true
- # user_data = "${file("../../shared/user-data.txt")}"
-  tags {
-    Name = "jembu-node"
-  }
+resource "aws_instance" "dse-node" {
+  # number of instances
+  count = 3
+
+  #Define Instance type - for free-tier refer AWS documentation
+  instance_type = "t2.xlarge"  
   
+  #Un-Comment (line 3 ,27 and below) if you want to use above created security group
+  #vpc_security_group_ids = [ "${aws_security_group.dse.id}" ]
+
+  # For Existing security group -Provide your AWS security group
+  security_groups=["sg-0e6bb77f8d9723052"]
+
+  #Add Exisitng keypair - SSH Connection with private key pair. if keypair not available, generate one through AWS console. Refer AWS documentation
+  key_name="jembukeypair"
+  
+  associate_public_ip_address = true
+  tags {
+    #Instance Tag name
+    Name = "dse-node - ${count.index}"
+  }
+
+  #Define image type - below for Ubuntu 14.4
   ami = "ami-0c54494f920b19106"
+
+  #Define availability zone  - For multiple region, use loop with index variable
   availability_zone = "us-east-1b"
+
+  #Subnet Id associated with your VPC
   subnet_id = "subnet-0e79be898021453fb"
 }
